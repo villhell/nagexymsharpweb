0a41327363710d429e37e6324ccaecceb896ed3e
villhell <lilly.of.the.valley.to@gmail.com>
2023-02-10 22:00:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Excels/ExcelManager.cs
@@ -0,0 +1,66 @@
+﻿using AntDesign.TableModels;
+using ClosedXML.Excel;
+using nagexymsharpweb.Models;
+using System.Reflection;
+
+namespace nagexymsharpweb.Excels
+{
+    /// <summary>
+    /// エクセルを管理するクラス
+    /// </summary>
+    public class ExcelManager
+    {
+        /// <summary>
+        /// ファイル名
+        /// </summary>
+        private string _fileName;
+        
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ExcelManager(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public List<DataItem> ReadExcelFile()
+        {
+            var excelRowDataItems = new List<DataItem>();
+            try
+            {
+                // 行数、先頭行はヘッダーなので2行目から解析する
+                int row = 2;
+                int key = 1;
+                using (var wb = new XLWorkbook(_fileName))
+                {
+                    foreach(var ws in wb.Worksheets)
+                    {
+                        while (row <= ws.LastRowUsed().RowNumber())
+                        {
+                            var rowdataItem = new DataItem
+                            {
+                                Key = key.ToString(),
+                                Check = string.Empty,
+                                Name = ws.Cell(row, 1).GetString(),
+                                Twitter = ws.Cell(row, 2).GetString(),
+                                Namespace = string.Empty,
+                                Address = ws.Cell(row, 5).GetString(),
+                                Xym = ws.Cell(row, 7).GetDouble(),
+                                Message = ws.Cell(row, 7).GetString()
+                            };
+
+                            row++;
+                            key++;
+                            excelRowDataItems.Add(rowdataItem);
+                        }
+                    }
+                };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return excelRowDataItems;
+        }
+    }
+}
---
+++ Models/DataItem.cs
@@ -0,0 +1,14 @@
+﻿namespace nagexymsharpweb.Models
+{
+    public class DataItem
+    {
+        public string Key { get; set; }
+        public string Check { get; set; }
+        public string Name { get; set; }
+        public string Twitter { get; set; }
+        public string Namespace { get; set; }
+        public string Address { get; set; }
+        public double Xym { get; set; }
+        public string Message { get; set; }
+    }
+}
